73ae863e5dc296bbc1d1bfcb79543abd58b7c323
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-03-03 15:27:07 +0100

    Changed Last output

--- src/main/java/TransformFunctions/PipeToLast.java
+++ src/main/java/TransformFunctions/PipeToLast.java
@@ -9,7 +9,7 @@ public class PipeToLast implements Function<JavaRDD<String>, JavaRDD<String>> {
     public JavaRDD<String> call(JavaRDD<String> read) throws Exception {
 
         //fasta input
-        String lastCall = "lastal -F15 /vol/Ma_Data_new/argdb/argdb";
+        String lastCall = "lastal -F15 /vol/Ma_Data_new/argdb/argdb -f BlastTab";
         JavaRDD<String> pipeRDD = read.pipe(lastCall);
         pipeRDD.collect();
         return pipeRDD;
