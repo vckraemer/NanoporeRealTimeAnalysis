3a145d3251f5800f7595459d9eb445cd4e6e3cf8
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-03-04 11:16:09 +0100

    Configured elasticsearch indexing

--- src/main/java/Streaming.java
+++ src/main/java/Streaming.java
@@ -46,7 +46,8 @@ public class Streaming {
         JavaDStream<String> lastResults = savedReads.transform(new PipeToLast());
         JavaDStream<String> resultStream = lastResults.map(new GetLastResults()).filter(x -> x!=null);
         JavaDStream<LastResult> endResults = resultStream.map(new ToLastResult());
-        JavaEsSparkStreaming.saveToEs(resultStream, "lastresults", ImmutableMap.of("es.mapping.id","queryName"));
+        JavaEsSparkStreaming.saveToEs(resultStream, "lastresults");
+        //ImmutableMap.of("es.mapping.id","queryName")
         //endResults.print();
         //lastResults.print();
         //lastResults.dstream().saveAsTextFiles("/vol/Ma_Data_new/lasttestresults", "txt");
