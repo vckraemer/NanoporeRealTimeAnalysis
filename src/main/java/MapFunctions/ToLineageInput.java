fe17600dc1c6bd37f263b822b7087f08a8b4c5e4
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-03-11 20:57:04 +0100

    Debugging taxon id to lineage script call.

--- src/main/java/MapFunctions/ToLineageInput.java
+++ src/main/java/MapFunctions/ToLineageInput.java
@@ -6,6 +6,6 @@ import org.apache.spark.api.java.function.Function;
 public class ToLineageInput implements Function<CentrifugeResult, String> {
     @Override
     public String call(CentrifugeResult cr) throws Exception {
-        return cr.getId()+ "\t" +cr.getTaxID()+"\n";
+        return cr.getId()+ '\t' +cr.getTaxID()+'\n';
     }
 }
