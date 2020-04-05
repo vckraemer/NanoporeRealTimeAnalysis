894277ea2a97c0e1165db58f7b13f53ca128db10
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-04-05 23:29:46 +0200

    Modified project for cluster usage: fixed monitored Folder

--- src/main/java/Streaming.java
+++ src/main/java/Streaming.java
@@ -27,7 +27,7 @@ public class Streaming {
         conf.set("es.resource", "sparkstreaming");
         conf.set("es.nodes.wan.only", "true");
         JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(10000));
-        JavaDStream<String> stream = ssc.textFileStream("/vol/Ma_Data/sequences");
+        JavaDStream<String> stream = ssc.textFileStream("/vol/spool/sequences");
         //JavaDStream<String> stream = ssc.textFileStream("/home/vanessa/Masterarbeit/workdir/sequences/");
 
 
