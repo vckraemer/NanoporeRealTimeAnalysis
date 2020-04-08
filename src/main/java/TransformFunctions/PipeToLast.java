f39dc82fa8bc8de66308b5329e6395558510dc72
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-04-08 19:33:35 +0200

    Fixed LAST call.

--- src/main/java/TransformFunctions/PipeToLast.java
+++ src/main/java/TransformFunctions/PipeToLast.java
@@ -9,7 +9,7 @@ public class PipeToLast implements Function<JavaRDD<String>, JavaRDD<String>> {
     public JavaRDD<String> call(JavaRDD<String> read) throws Exception {
 
         //fasta input
-        String lastCall = "lastal -p 8 -F15 -f BlastTab /home/ubuntu/arg_annot_db/arg_annot_db ";
+        String lastCall = "lastal -P 8 -F15 -f BlastTab /home/ubuntu/arg_annot_db/arg_annot_db ";
         //String lastCall = "lastal -F15 /home/vanessa/Masterarbeit/workdir/lastdb/argdb -f BlastTab";
 
         JavaRDD<String> pipeRDD = read.pipe(lastCall);
