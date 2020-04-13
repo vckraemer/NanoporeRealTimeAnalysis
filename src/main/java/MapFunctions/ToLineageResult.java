719c0dfc4ff6c44cad3fbe993b565fbd2a0f5845
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-04-13 22:19:28 +0200

    Added fix for null id in lineage results.

--- src/main/java/MapFunctions/ToLineageResult.java
+++ src/main/java/MapFunctions/ToLineageResult.java
@@ -11,7 +11,11 @@ public class ToLineageResult implements Function<String, LineageResults> {
 
         String[] fields = s.split("\t");
         if(fields.length==13){
-            return new LineageResults(fields[0], fields[1], fields[5], fields[6], fields[7], fields[8],fields[9], fields[10], fields[11], fields[12]);
+            if(!fields[1].equals("")) {
+                return new LineageResults(fields[0], fields[1], fields[5], fields[6], fields[7], fields[8], fields[9], fields[10], fields[11], fields[12]);
+            }else {
+                return null;
+            }
         }else {
             return null;
         }
