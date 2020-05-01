49309b2073ebe626e790dbb49c877059c695e2e8
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-05-01 20:25:45 +0200

    Fixed LineageToTaxonomyWrapper

--- Wrapper/centrifugeWrapper.sh
+++ Wrapper/centrifugeWrapper.sh
@@ -3,10 +3,12 @@
 filename=$1
 savefile=$filename"reads.fasta"
 
-cd /home/ubuntu/vol/tmp_results/
+cd /home/ubuntu/vol/tmp_results/ || { echo "Failure"; exit 1; }
 
 while read LINE; do
-   echo "${LINE}" >> $savefile
+   echo "${LINE}" >> "$savefile"
 done
 
-centrifuge -f -k 1 -p 8 -x /home/ubuntu/vol/spool/p_compressed+h+v/p_compressed+h+v $savefile
+centrifuge -f -k 1 -p 8 -x /home/ubuntu/vol/spool/p_compressed+h+v/p_compressed+h+v "$savefile"
+
+rm -f "$savefile"
--- Wrapper/taxonomyToLineageWrapper.sh
+++ Wrapper/taxonomyToLineageWrapper.sh
@@ -3,11 +3,14 @@
 VAR=""
 savefile=$1"LineageInput.txt"
 
-cd /home/ubuntu/vol/tmp_results
+cd /home/ubuntu/vol/tmp_results/ || { echo "Failure"; exit 1; }
+
 while read LINE; do
-	echo "${LINE}" >> $savefile
+	echo "${LINE}" >> "$savefile"
 done
 
-cd /home/ubuntu/
+cd /home/ubuntu/ || { echo "Failure"; exit 1; }
 
-./megan_taxon2lineage.pl -db taxonomy/ -f /home/ubuntu/vol/tmp_results/$savefile
+./megan_taxon2lineage.pl -db taxonomy/ -f /home/ubuntu/vol/tmp_results/"$savefile"
+
+rm -f /home/ubuntu/vol/tmp_results/"$savefile"
