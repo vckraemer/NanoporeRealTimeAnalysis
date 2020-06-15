cb3996657d15aac865dc357cee528013b6ed363a
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-06-15 22:19:22 +0200

    New lineage detection

--- Scripts/etelineage.py
+++ Scripts/etelineage.py
@@ -2,16 +2,19 @@
 #-*- coding: utf-8 -*-
 from ete3 import NCBITaxa
 import sys
+import re
 
 ncbi = NCBITaxa()
 lineage_dict = {}
 
 for line in sys.stdin:
+        if ";" in line:
             ids = line.split(';')
-            lineage_dict[ids[0]] = ids[1].strip()
+            if len(ids) is 2:
+                lineage_dict[ids[0]] = ids[1].strip()
 
 for key in lineage_dict:
-        if lineage_dict[key] is not "0":
+        if re.match(r"^[1-9][0-9]*$",lineage_dict[key]):
             lineage = ncbi.get_lineage(lineage_dict[key])
             names = ncbi.get_taxid_translator(lineage)
             lineagenames = '|'.join(str(x) for x in names.values())
