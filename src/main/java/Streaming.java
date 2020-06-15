94157f70bcc209a2a9ac0f6eb8371e88000f85ea
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-06-15 15:09:13 +0200

    Optimizing performance

--- src/main/java/Streaming.java
+++ src/main/java/Streaming.java
@@ -81,9 +81,11 @@ public class Streaming {
 
         savedReads.context().sparkContext().setLocalProperty("spark.scheduler.pool", "fair_pool");
         JavaDStream<String> centrifugeResults = savedReads.transform(new PipeToCentrifuge(centrifugeDatabasePath));
-        JavaDStream<CentrifugeResult> endResult = centrifugeResults.map(new ToCentrifugeResult()).filter(x -> x!=null).transform(new SaveCentrifugeResultsToElastic(esIndexPrefix));
-        //endResult.context().sparkContext().setLocalProperty("spark.scheduler.pool", "a_different_pool");
-        JavaDStream<LineageResult> lineage = endResult.map(new ToLineageInput()).filter(x -> x!=null).transform(new PipeToTaxonomy2Lineage()).map(new ToLineageResult()).filter(x -> x!=null);
+        JavaDStream<CentrifugeResult> endResult = centrifugeResults.map(new ToCentrifugeResult()).filter(x -> x!=null);
+        JavaDStream<CentrifugeResult> savedResults = endResult.cache();
+        JavaEsSparkStreaming.saveToEs(savedResults, esIndexPrefix+"centrifugeresults");
+
+        JavaDStream<LineageResult> lineage = savedResults.map(new ToLineageInput()).filter(x -> x!=null).transform(new PipeToTaxonomy2Lineage()).map(new ToLineageResult()).filter(x -> x!=null);
         JavaEsSparkStreaming.saveToEs(lineage, esIndexPrefix+"lineageresults");
 
         //JavaDStream<String> blastResults = savedReads.transform(new PipeToBlast()).map(new GetBlastResultJsonSingleReport()).filter(x -> x!=null);
