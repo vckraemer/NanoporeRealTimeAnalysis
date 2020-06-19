adc4be5647bde1e4553cf8ea2031c0b89a1716d6
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-06-19 20:20:42 +0200

    Set batch duration to one second and added repartitioning to parallelize execution.

--- src/main/java/TransformFunctions/PipeToCentrifuge.java
+++ src/main/java/TransformFunctions/PipeToCentrifuge.java
@@ -4,6 +4,8 @@ import org.apache.spark.TaskContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 
+import java.time.LocalDate;
+
 public class PipeToCentrifuge implements Function<JavaRDD<String>, JavaRDD<String>> {
 
     private String selectedDatabase;
@@ -19,7 +21,7 @@ public class PipeToCentrifuge implements Function<JavaRDD<String>, JavaRDD<Strin
         String[] debugParts = debug.split("\\|");
         String path = debugParts[debugParts.length-1].split(" ")[2].replace("file:", "");
         String[] pathParts = path.split("/");
-        String filename = pathParts[pathParts.length-1] + TaskContext.getPartitionId();
+        String filename = pathParts[pathParts.length-1] + LocalDate.now().toString();
 
         //-q fastq input -f fasta input
         String centrifugeCall = "bash /home/ubuntu/centrifugeWrapper.sh " + filename + " " + selectedDatabase;
