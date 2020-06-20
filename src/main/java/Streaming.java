062f35add9a2c66298716d1e4ad564652199781c
vckraemer <vkraemer@cebitec.uni-bielefeld.de>
2020-06-20 17:11:05 +0200

    Added custom thread settings for LAST and  Centrifuge.

--- src/main/java/Streaming.java
+++ src/main/java/Streaming.java
@@ -86,7 +86,7 @@ public class Streaming {
         conf.set("es.nodes.wan.only", "true");
         conf.set("mapred.max.split.size","10000000");
 
-        JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(30000));
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(10000));
 
         JavaPairInputDStream<LongWritable, Text> fastqRDD =  ssc.fileStream(folderPath, LongWritable.class, Text.class, FastqInputFormat.class);
         JavaDStream<Read> reads = fastqRDD.map(Tuple2::_2).map(new TextToString()).map(new ToReadObject()).filter(x -> x!=null).map(new CalculateGCContent()).transform(new SaveToElastic(esIndexPrefix));
